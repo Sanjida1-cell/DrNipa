4a8d87fa53564f3662f560e886841bcf82c93065
Sanjida <sanjidasanju0131@gmail.com>
2025-05-08 18:32:44 +0600

    initial commit

---
+++ main.go
@@ -0,0 +1,80 @@
+// main.go (Go backend for appointment booking)
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"sync"
+)
+
+type Appointment struct {
+	Name  string `json:"name"`
+	Phone string `json:"phone"`
+	Date  string `json:"date"`
+	Time  string `json:"time"`
+}
+
+var (
+	appointments []Appointment
+	mu           sync.Mutex
+)
+
+func bookAppointment(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Only POST method is allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var a Appointment
+
+	// Check content type to support HTMX form submission
+	contentType := r.Header.Get("Content-Type")
+	if contentType == "application/json" {
+		err := json.NewDecoder(r.Body).Decode(&a)
+		if err != nil {
+			http.Error(w, "Invalid JSON input", http.StatusBadRequest)
+			return
+		}
+	} else {
+		err := r.ParseForm()
+		if err != nil {
+			http.Error(w, "Invalid form input", http.StatusBadRequest)
+			return
+		}
+		a = Appointment{
+			Name:  r.FormValue("name"),
+			Phone: r.FormValue("phone"),
+			Date:  r.FormValue("date"),
+			Time:  r.FormValue("time"),
+		}
+	}
+
+	mu.Lock()
+	appointments = append(appointments, a)
+	mu.Unlock()
+
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "text/plain")
+	w.Write([]byte("Appointment booked successfully"))
+}
+
+func getAppointments(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	defer mu.Unlock()
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(appointments)
+}
+
+func main() {
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "static/index.html")
+	})
+	http.HandleFunc("/book", bookAppointment)
+	http.HandleFunc("/appointments", getAppointments)
+
+	log.Println("Server running on http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
